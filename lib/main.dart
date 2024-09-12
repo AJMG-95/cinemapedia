42d7191c9aa0b889cfa1c81b47120ca9342b1868
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-09-12 20:57:32 +0200

    feat: install riverpod & implements movieRepositoryProvider

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MainApp());
+  //Riverpod exige hacer una configuración en el runApp del mani() <- main.dart
+  runApp(
+      // Es neceario que se use el ProviderScope, que es el que va a contener las referncias
+      //  a todos los providers de riverpod
+    const ProviderScope(child: MainApp(),)
+  );
 }
 
 class MainApp extends StatelessWidget {
---
+++ lib/presentation/providers/movies/movie_repository_provider.dart
@@ -0,0 +1,21 @@
+//* Este archivo crea la instancia del repository
+
+// Como esto va a ser un provider que no se va a modificar (inmutable), entidase como
+//  tal que el estado/datos que contiene nunca cambiar, y por ello se puede crear
+//  de/para solo lectura
+// EL objetico de esta clase es proporcionar el MoviesRepositoryImpl de manera global,
+//  para que cualquier otro provider tenga acceso a esa información
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+
+// Como es de solo lectura se usa el tipo/método Provier
+final movieRepositoryProvider = Provider((ref) {
+  // Este provider debe devolver la ikmplemetaciín del repository y este
+  //  repository pide que se le pase como argumentro el datasource, que es el origen de datos
+  //  de la aplicació, por lo que se le pasa la implementación específica de lo que se va a
+  //  necesitar para extraer la infromación
+  return MoviesRepositoryImpl( MoviedbDatasource() ) ;
+});
