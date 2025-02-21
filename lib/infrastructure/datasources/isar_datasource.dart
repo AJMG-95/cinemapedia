0c636794136de30e289f7b3919303744ed5117b8
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2025-02-21 16:45:31 +0100

    feat:añadir/quitar de favoritos

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,36 +1,51 @@
-import 'package:path_provider/path_provider.dart' show getApplicationDocumentsDirectory;
-
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
 
+///! Clase `IsarDatasource`
+/// Esta clase implementa `LocalStorageDatasource` y gestiona el almacenamiento
+/// local de las películas favoritas utilizando la base de datos Isar.
 class IsarDatasource extends LocalStorageDatasource {
+  ///? `db` es una instancia futura de Isar que almacena la base de datos local.
   late Future<Isar> db;
 
+  ///* Constructor
+  /// Inicializa la base de datos llamando a `openDB()`.
   IsarDatasource() {
     db = openDB();
   }
 
+  ///! Método `openDB`
+  ///* Abre o recupera una instancia de la base de datos Isar.
+  ///* Si no hay ninguna instancia abierta, la crea y la almacena en el directorio de documentos.
   Future<Isar> openDB() async {
     if (Isar.instanceNames.isEmpty) {
-      final dir = await getApplicationDocumentsDirectory();
+      final dir =
+          await getApplicationDocumentsDirectory(); //? Obtiene el directorio donde se almacenará la BD.
       return await Isar.open([MovieSchema],
           inspector: true, directory: dir.path);
     }
-
-    return Future.value(Isar.getInstance());
+    return Future.value(
+        Isar.getInstance()); //? Retorna la instancia existente de la BD.
   }
 
+  ///! Método `isMovieFavorite`
+  ///* Comprueba si una película está marcada como favorita en la base de datos.
   @override
   Future<bool> isMovieFavorite(int movieId) async {
     final isar = await db;
-
     final Movie? isFavoriteMovie =
         await isar.movies.filter().idEqualTo(movieId).findFirst();
-
-    return isFavoriteMovie != null;
+    return isFavoriteMovie !=
+        null; //? Retorna `true` si la película existe en la BD, `false` en caso contrario.
   }
 
+  ///! Método `toggleFavorite`
+  ///* Añade o elimina una película de la lista de favoritos.
+  /// Si la película ya está en favoritos, la elimina.
+  /// Si no está, la añade.
   @override
   Future<void> toggleFavorite(Movie movie) async {
     final isar = await db;
@@ -39,19 +54,26 @@ class IsarDatasource extends LocalStorageDatasource {
         await isar.movies.filter().idEqualTo(movie.id).findFirst();
 
     if (favoriteMovie != null) {
-      // Borrar
+      //! Eliminar película de favoritos
       isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
       return;
     }
 
-    // Insertar
+    //* Agregar película a favoritos
     isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 
+  ///! Método `loadMovies`
+  ///* Carga una lista de películas desde la base de datos.
+  /// - `limit`: Cantidad máxima de películas a recuperar.
+  /// - `offset`: Número de registros a saltar.
   @override
   Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
     final isar = await db;
-
-    return isar.movies.where().offset(offset).limit(limit).findAll();
+    return isar.movies
+        .where()
+        .offset(offset)
+        .limit(limit)
+        .findAll(); //? Retorna una lista de películas.
   }
 }
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -240,13 +240,38 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
-class _CustomSliverAppBar extends StatelessWidget {
+///! `isFavoriteProvider` OJO este provider está aquí porque es pequeño y solo se usa aquí
+///* Este `FutureProvider.family.autoDispose` se encarga de verificar si una película está marcada como favorita.
+/*
+?`family`:
+  - `family` permite que el provider acepte un parámetro dinámico (en este caso, `movieId`).
+  - Cada valor único de `movieId` genera una instancia distinta del provider.
+  - Útil cuando queremos manejar múltiples valores simultáneamente sin que se sobrescriban entre sí.
+*/
+/*
+?`autoDispose`:
+  - `autoDispose` asegura que cuando el provider ya no se use, se elimine automáticamente.
+  - Ayuda a liberar memoria y evitar fugas de estado innecesarias.
+  - Se destruye cuando no hay más widgets observando el provider, evitando mantener datos en memoria más tiempo del necesario.
+*/
+///* - `ref.watch(localStorageRepositoryPrivder)`: Obtiene el repositorio local de almacenamiento.
+///* - `isMovieFavorite(movieId)`: Retorna un `Future<bool>` indicando si la película es favorita o no.
+final isFavoriteProvider =
+    FutureProvider.family.autoDispose((ref, int movieId) {
+  final localStorageRepository = ref.watch(localStorageRepositoryPrivder);
+  return localStorageRepository.isMovieFavorite(movieId);
+});
+
+
+class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
 
   const _CustomSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue isFavoriteFuture = ref.watch(isFavoriteProvider(movie.id));
+
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -254,9 +279,32 @@ class _CustomSliverAppBar extends StatelessWidget {
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
       actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)
-            /* icon: Icon(Icons.favorite_rounded, color: Colors.red,) */
-            )
+        //! `IconButton` para marcar o desmarcar una película como favorita.
+        //? - `ref.watch(localStorageRepositoryPrivder).toggleFavorite(movie)`: Alterna el estado de favorito.
+        //? - `ref.invalidate(isFavoriteProvider(movie.id))`: Invalida el estado para forzar una actualización visual.
+        IconButton(
+            onPressed: () {
+              ref.watch(localStorageRepositoryPrivder).toggleFavorite(movie);
+
+              ref.invalidate(isFavoriteProvider(movie.id));
+            },
+            icon: isFavoriteFuture.when(
+              loading: () => const CircularProgressIndicator(
+                strokeWidth: 2,
+              ),
+              data: (isFavorite) => isFavorite
+                  ? const Icon(
+                      Icons.favorite_rounded,
+                      color: Colors.red,
+                    )
+                  : const Icon(Icons.favorite_border),
+              /*
+                ? Manejo de errores con `UnimplementedError()`:
+                - Este error se lanza si ocurre un fallo en el provider al obtener el estado de favorito.
+                - En un escenario real, se debería manejar el error de una forma más adecuada, como mostrando un mensaje en pantalla.
+              */
+              error: (_, __) => throw UnimplementedError(),
+            ))
       ],
       flexibleSpace: FlexibleSpaceBar(
         /* titlePadding: const EdgeInsets.only(left: 10, bottom: 7),
@@ -278,22 +326,34 @@ class _CustomSliverAppBar extends StatelessWidget {
               ),
             ),
             const _CustomGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              stops: [0.0, 0.2],
-              colors: [Colors.black54,Colors.transparent,]
-            ),
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                stops: [
+                  0.0,
+                  0.2
+                ],
+                colors: [
+                  Colors.black54,
+                  Colors.transparent,
+                ]),
             const _CustomGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [0.8, 1.0],
-              colors: [Colors.transparent, Colors.black54,]
-            ),
-            const _CustomGradient(
-              begin: Alignment.topLeft,
-              stops: [0.0, 0.3],
-              colors: [Colors.black87, Colors.transparent,]
-            ),
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [
+                  0.8,
+                  1.0
+                ],
+                colors: [
+                  Colors.transparent,
+                  Colors.black54,
+                ]),
+            const _CustomGradient(begin: Alignment.topLeft, stops: [
+              0.0,
+              0.3
+            ], colors: [
+              Colors.black87,
+              Colors.transparent,
+            ]),
           ],
         ),
       ), // Espacioflexible del customAppBar
@@ -307,26 +367,19 @@ class _CustomGradient extends StatelessWidget {
   final List<double> stops;
   final List<Color> colors;
 
-  const _CustomGradient({
-    this.begin = Alignment.centerLeft,
-    this.end = Alignment.centerRight,
-    required this.stops,
-    required this.colors
-  });
+  const _CustomGradient(
+      {this.begin = Alignment.centerLeft,
+      this.end = Alignment.centerRight,
+      required this.stops,
+      required this.colors});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
       child: DecoratedBox(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: begin,
-              end: end,
-              stops: stops,
-              colors: colors
-            )
-          )
-        ),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: begin, end: end, stops: stops, colors: colors))),
     );
   }
 }
